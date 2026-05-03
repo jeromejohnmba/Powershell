08fc9eca9bcb91f9ba7f3eb16febb9732922b8cc
jjohn92085 <jjohn92085@gmail.com>
2026-05-03 00:22:19 -0700

    creates multiple users

---
+++ Multiple-users/multiple-users.ps1
@@ -0,0 +1,33 @@
+
+
+# creates multiple local users
+# @() creates an array of usernames
+# ReadHost stores password securely once it is entered
+
+$usernames = @("James", "Chris")
+$password = Read-Host -AsSecureString "Enter default password"
+$usernames | ForEach -Object { New-LocalUser -Name $_ $password}
+
+# create a local user without a password
+# expire user account
+# account expires after 30 days after calculating the date from Get-Date
+
+New-LocalUser -Name 'Jason' -AccountExpires (Get-Date).AddDays(30) -Description 'This is a local user account.' -NoPassword
+
+#create a local user account with a password
+
+$Password = Read-Host -AsSecureString
+$params = @{
+    Name = 'new user'
+    Password = $password
+    FullName = 'Jason Redson'
+    Description = 'This is a new account for Jason'
+}
+
+New-LocalUser @params
+
+# Expires an existing user 
+
+Set-LocalUser -Name "ExistingUser" -AccountExpires (Get-Date).AddDays(7)
+
+
