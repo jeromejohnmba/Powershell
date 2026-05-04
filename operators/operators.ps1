3a6acbe2902a4e101f1b5cf566affad2b648cbbf
jjohn92085 <jjohn92085@gmail.com>
2026-05-04 13:24:14 -0700

    operators

---
+++ operators/operators.ps1
@@ -0,0 +1,31 @@
+#test of operators
+
+# add together names
+$name = "Chris"  + " " + "Smith"
+Write-Host $name
+
+#repeat name multiple times
+
+$repeatName = "Chris " * 3
+Write-Host $repeatName
+
+#addition
+$add = 3 + 3
+Write-Host $add
+
+#subtract
+$subtract = 2 - 1 
+Write-host $subtract
+
+#divide
+$divide = 5 / 5
+Write-Host $divide
+
+#multiply
+$multiply = 4 * 4
+Write-Host $multiply
+
+#modulo
+$modulo = 30 % 5
+Write-Host $modulo
+
