b34d67d7594203bfab01c945bf513178f26533c5
jjohn92085 <jjohn92085@gmail.com>
2026-05-03 23:04:24 -0700

    multiple variable practice

---
+++ multiple-usernames/multiple-usernames.ps1
@@ -0,0 +1,29 @@
+
+#create multiple usernames
+
+$firstUser, $secondUser, $thirdUser = "Chris", "Bob", "Mike"
+
+$UserList = $firstUser, $secondUser, $thirdUser
+
+# create common password
+
+$Password = Read-Host -AsSecureString "Enter password here"
+
+forEach ($User in $UserList) {
+
+    # creates the user
+
+    New-LocalUser -Name $User -Password $Password -Description "This is the account for $User" -FullName $User
+
+    # add user to local group 
+
+    Add-LocalGroupMember -Group "Users" -Member $User
+
+}
+
+
+
+
+
+
+
